afabcc2c9d3859db5eb2a4a08046da4a004a0cd5
ajprasadd <103500605+ajprasadd@users.noreply.github.com>
2025-01-26 14:15:14 +0530

    Update adv.py

--- adv.py
+++ adv.py
@@ -4,7 +4,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
